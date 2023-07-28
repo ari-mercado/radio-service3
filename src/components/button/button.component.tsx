9e9f5b25b49bed6a167e1e258e82b44069d5c9be
ari <mercado7946@gmail.com>
2023-07-28 15:08:01 -0700

    Implement Footer and month Button

---
+++ src/components/button/button.component.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './button.module.scss';
+import { AiOutlineDoubleRight } from 'react-icons/ai';
+
+interface ButtonProps {
+  handleSetActive: (player: string) => any;
+  audioTitle: string;
+}
+
+const Button = ({ handleSetActive, audioTitle }: ButtonProps) => {
+  return (
+    <button
+      className={styles.button}
+      onClick={() => handleSetActive(audioTitle)}
+    >
+      <span className={styles.buttonText}>{audioTitle}</span>
+      <AiOutlineDoubleRight className={styles.icon} />
+    </button>
+  );
+};
+
+export default Button;

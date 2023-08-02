90238bd1be4179b9afa880a4b77ce0a1f8d29764
ari <mercado7946@gmail.com>
2023-08-02 16:10:33 -0700

    Make player list a client component to use useParams hook in order to fetch the the correct audio data

--- src/components/player-list/player-list.component.tsx
+++ src/components/player-list/player-list.component.tsx
@@ -1,17 +1,32 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
 import AudioPlayer from './player.component';
 import styles from './player-list.module.scss';
 import { AudioData } from '../../types/types';
 
 interface PlayerListProps {
-  audioData: AudioData[];
+  audioData?: AudioData[];
 }
 
-const PlayerList = ({ audioData }: PlayerListProps) => {
-  const firstPlayer = audioData && audioData[0]?.audioTitle;
-  const [isActive, setIsActive] = React.useState(firstPlayer || '');
+const PlayerList = () => {
+  const [isActive, setIsActive] = React.useState('');
+  const [audioData, setAudioData] = useState([]);
+  const params = useParams();
+  const currentMonth = params.months as string;
+
+  useEffect(() => {
+    const fetchAudio = async () => {
+      const res = await fetch(process.env.NEXT_PUBLIC_FOURTEEN_DATA_URL);
+      const data = await res.json();
+      const { audioData } = data?.fourteen?.monthsData[currentMonth];
+      // const audioDatas = data?.fourteen?.monthsData?.enero?.audioData;
+      setAudioData(audioData);
+    };
+
+    fetchAudio();
+  }, [currentMonth]);
 
   const handleSetActive = (activePlayer: string) => {
     setIsActive(activePlayer);

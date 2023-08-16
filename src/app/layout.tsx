f5abd4eafcbac0396de8919db18460f971e7829f
ari <mercado7946@gmail.com>
2023-08-16 15:23:48 -0700

    add google fonts and minor changes to UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,20 @@ import { Metadata } from 'next';
 import './globals.css';
 import Header from '../components/header/header.component';
 import Footer from '@/components/footer/footer.component';
+import { Poppins, Lobster } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: '300',
+  style: 'normal',
+  variable: '--font-poppins',
+});
+
+const lobster = Lobster({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-lobster',
+});
 
 export const metadata: Metadata = {
   title: `Tu Hora Divina`,
@@ -14,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable} ${lobster.variable}`}>
       <body>
         <Header />
         {children}

a4b3d76432d2eb8e64dc9df68888043db783bde2
ari <mercado7946@gmail.com>
2023-10-06 11:30:55 -0700

    Remove commented out code and unused files

--- src/components/player-list/player-list.component.tsx
+++ src/components/player-list/player-list.component.tsx
@@ -1,13 +1,9 @@
-// 'use client';
-
-// import React, { useState, useEffect } from 'react';
-// import { useParams } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import styles from './player-list.module.scss';
 const AudioPlayer = dynamic(() => import('../player/player.component'), {
+  // disable server prerendering to prevent hydration errors
   ssr: false,
 });
-// import { AudioData } from '../../types/types';
 import { transformCollectionToMonth } from '@/utils/utils';
 
 const getAudioData = async (currentMonth) => {
@@ -19,27 +15,7 @@ const getAudioData = async (currentMonth) => {
 };
 
 const PlayerList = async ({ month }: { month: string }) => {
-  // const [isActive, setIsActive] = useState('');
-  // const [audioData, setAudioData] = useState<AudioData[]>([]);
-  // const params = useParams();
   const audioData = await getAudioData(month);
-
-  // useEffect(() => {
-  //   const fetchAudio = async () => {
-  //     const res = await fetch(process.env.NEXT_PUBLIC_FOURTEEN_DATA_URL);
-  //     const data = await res.json();
-  //     const currentMonth = params.months as string;
-  //     const formattedMonth = transformCollectionToMonth[currentMonth];
-  //     const { audioData } = data?.fourteen?.monthsData[formattedMonth];
-  //     setAudioData(audioData);
-  //   };
-
-  //   fetchAudio();
-  // }, [params.months]);
-
-  // const handleSetActive = (activePlayer: string) => {
-  //   setIsActive(activePlayer);
-  // };
 
   return (
     <div className={styles.playerWrapper}>
